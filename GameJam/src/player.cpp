47368495a64c54f658fefad273aa1e640453c368
Kian <33534924+creator-double@users.noreply.github.com>
2021-03-08 20:51:01 +0000

    im stupid

--- GameJam/src/player.cpp
+++ GameJam/src/player.cpp
@@ -11,28 +11,26 @@ void Player::movement()
 
     if ((m_pge->GetKey(olc::RIGHT).bPressed || m_pge->GetKey(olc::RIGHT).bHeld))
     {
-      std::cout << "down\n";
+      std::cout << "right\n";
     }
 
     if ((m_pge->GetKey(olc::UP).bPressed || m_pge->GetKey(olc::UP).bHeld))
     {
-      std::cout << "lup\n";
+      std::cout << "up\n";
     }
 
     if ((m_pge->GetKey(olc::DOWN).bPressed || m_pge->GetKey(olc::DOWN).bHeld))
     {
       std::cout << "down\n";
     }
-
-    if (m_pge->GetKey(olc::SPACE).bPressed)
-    {
-      std::cout << "interact\n";
-    }
 }
 
 void Player::interaction()
 {
-  
+  if (m_pge->GetKey(olc::SPACE).bPressed)
+  {
+    std::cout << "interact\n";
+  }
 }
 
 void Player::render()
